764ede39dd5946a53a773f2d75db9c593adbf64d
Bruno Brito <brunopbrito31@hotmail.com>
2021-05-21 03:35:51 -0300

    pesquisa por 1 pais especifico adicionada

--- src/main/java/com/github/brunopbrito/citiesapi/countries/resources/CountryResource.java
+++ src/main/java/com/github/brunopbrito/citiesapi/countries/resources/CountryResource.java
@@ -3,10 +3,12 @@ package com.github.brunopbrito.citiesapi.countries.resources;
 import com.github.brunopbrito.citiesapi.countries.entities.Country;
 import com.github.brunopbrito.citiesapi.countries.repositories.CountryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -22,5 +24,11 @@ public class CountryResource {
     @GetMapping
     public List<Country> countries(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Country getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+        return optional.get();
     }
 }
