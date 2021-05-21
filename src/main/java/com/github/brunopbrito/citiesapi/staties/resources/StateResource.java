8fbc2d0f687b0bf047ff882af0d098347cafd7c9
Bruno Brito <brunopbrito31@hotmail.com>
2021-05-21 04:44:34 -0300

    Busca por apenas 1 estado adicionada

--- src/main/java/com/github/brunopbrito/citiesapi/staties/resources/StateResource.java
+++ src/main/java/com/github/brunopbrito/citiesapi/staties/resources/StateResource.java
@@ -3,10 +3,12 @@ package com.github.brunopbrito.citiesapi.staties.resources;
 import com.github.brunopbrito.citiesapi.staties.entities.State;
 import com.github.brunopbrito.citiesapi.staties.repositories.StateRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/staties")
@@ -18,8 +20,14 @@ public class StateResource {
         this.repository = repository;
     }
 
-    @GetMapping
+    @GetMapping()
     public List<State> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<State> getOne(@PathVariable Long id){
+        Optional<State> optional = repository.findById(id);
+        return optional;
     }
 }
